e46cb90dd29632ef3fe3bf6f9153a0acb93480b5
Jeff Martinez <jeff@jeff-mac16.local>
2024-07-26 13:49:54 -0700

    initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,6 +5,8 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
+import { Modal } from '@/components/ui/modal';
+
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
